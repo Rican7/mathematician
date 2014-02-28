ecb8922f76d4fef7907a2d4b40a19b97f87a0195
Trevor Suarez <rican7@gmail.com>
2014-02-28 01:35:53 -0500

    New basic PHP unit testing framework

---
+++ tests/Mathematician/Test/AbstractMathematicianTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Mathematician
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2014 Trevor Suarez
+ * @license     MIT
+ * @version     0.1.0
+ */
+
+namespace Mathematician\Test;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * AbstractMathematicianTest
+ *
+ * Abstract base test class to make
+ * unit testing less of a pain
+ *
+ * @uses PHPUnit_Framework_TestCase
+ * @abstract
+ * @package Mathematician\Test
+ */
+abstract class AbstractMathematicianTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Properties
+     */
+
+    /**
+     * The directory containing the test files
+     *
+     * @var string
+     * @access private
+     */
+    private $tests_dir;
+
+
+    /**
+     * Methods
+     */
+
+    /**
+     * Setup our test
+     *
+     * @access protected
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // Get our tests directory
+        $this->tests_dir = dirname($GLOBALS['__PHPUNIT_BOOTSTRAP']);
+    }
+
+    /**
+     * Get the directory containing the test files
+     *
+     * @access protected
+     * @return string
+     */
+    protected function getTestsDirectory()
+    {
+        return $this->tests_dir;
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Mathematician
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2014 Trevor Suarez
+ * @license     MIT
+ * @version     0.1.0
+ */
+
+
+// Turn on all errors for our unit testing
+error_reporting(-1);
+
+// Load our autoloader, and add our Test class namespace
+$autoloader = require(__DIR__ . '/../vendor/autoload.php');
+$autoloader->add('Mathematician\Tests', __DIR__);
