fbd7e2e6e2942cad83daf62505359a392f1f86c5
Trevor Suarez <rican7@gmail.com>
2014-04-21 10:42:05 -0400

    Oooooops. Adding an exception that I never committed way back when I did the constructor redesign (in commit 343b80a534719a753bf0e33396e1c8c982b8f555)

---
+++ src/Mathematician/Exception/UnsupportedNumericFormatException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Mathematician
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2014 Trevor Suarez
+ * @license     MIT
+ * @version     0.1.0
+ */
+
+namespace Mathematician\Exception;
+
+use UnexpectedValueException;
+
+/**
+ * UnsupportedNumericFormatException
+ *
+ * @uses UnexpectedValueException
+ * @uses MathematicianExceptionInterface
+ * @package Mathematician\Exception
+ */
+class UnsupportedNumericFormatException extends UnexpectedValueException implements MathematicianExceptionInterface
+{
+
+    /**
+     * Default Properties
+     */
+
+    protected $message = 'The given numeric representation is unsupported';
+}
