d16b0d890aa857a2f405d7df707d12cb6cbbbfcb
Trevor Suarez <rican7@gmail.com>
2014-04-23 02:33:09 -0400

    New test class for the Number class

---
+++ tests/Mathematician/Test/NumberTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Mathematician
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2014 Trevor Suarez
+ * @license     MIT
+ * @version     0.1.0
+ */
+
+namespace Mathematician\Test;
+
+use Mathematician\Number;
+
+/**
+ * NumberTest
+ *
+ * @uses AbstractMathematicianTest
+ * @package Mathematician\Test
+ */
+class NumberTest extends AbstractMathematicianTest
+{
+
+    public function testIsGmpAvailable()
+    {
+        $available = Number::isGmpAvailable();
+
+        if (function_exists('gmp_init')) {
+            $this->assertTrue($available);
+        } else {
+            $this->assertFalse($available);
+        }
+    }
+
+    public function testIsBcMathAvailable()
+    {
+        $available = Number::isBcMathAvailable();
+
+        if (function_exists('bcadd')) {
+            $this->assertTrue($available);
+        } else {
+            $this->assertFalse($available);
+        }
+    }
+}
