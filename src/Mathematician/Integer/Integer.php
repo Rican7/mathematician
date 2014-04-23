807d18aebfba12dd599273750bee894bee82d050
Trevor Suarez <rican7@gmail.com>
2014-04-23 02:59:03 -0400

    Refactored the Number factory into a split factory just for Integers

---
+++ src/Mathematician/Integer/Integer.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Mathematician
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2014 Trevor Suarez
+ * @license     MIT
+ * @version     0.1.0
+ */
+
+namespace Mathematician\Integer;
+
+use Mathematician\Exception\AdapterSupportException;
+use Mathematician\Integer\Adapter\AdapterInterface;
+use Mathematician\Integer\Adapter\BcMath;
+use Mathematician\Integer\Adapter\Gmp;
+use Mathematician\Number;
+
+/**
+ * Integer
+ *
+ * @abstract
+ * @package Mathematician
+ */
+abstract class Integer
+{
+
+    /**
+     * Methods
+     */
+
+    /**
+     * Create an instance of an Integer adapter
+     *
+     * @param mixed $number
+     * @static
+     * @access public
+     * @throws AdapterSupportException
+     * @return AdapterInterface
+     */
+    public static function factory($number)
+    {
+        // Use GMP if we can... its MUCH faster
+        if (Number::isGmpAvailable()) {
+            return Gmp::factory($number);
+
+        } elseif (Number::isBcMathAvailable()) {
+            return BcMath::factory($number);
+        }
+
+        // TODO: Fall back to native?
+        throw new AdapterSupportException();
+    }
+}
--- src/Mathematician/Number.php
+++ src/Mathematician/Number.php
@@ -11,9 +11,7 @@
 namespace Mathematician;
 
 use Mathematician\Exception\AdapterSupportException;
-use Mathematician\Integer\Adapter\AdapterInterface;
-use Mathematician\Integer\Adapter\BcMath;
-use Mathematician\Integer\Adapter\Gmp;
+use Mathematician\Integer\Integer;
 
 /**
  * Number
@@ -58,23 +56,16 @@ abstract class Number
      */
     public static function factory($number, $scale = 0)
     {
-        // If we have a float or scale set, then use BcMath so
-        // we don't lose the ability to work on float values
-        if ((is_float($number) || ((int) $scale) != 0)
-            && static::isBcMathAvailable()) {
+        if (is_float($number) || ((int) $scale) !== 0) {
+            // TODO: Floating-point/decimal adapter
+            throw new AdapterSupportException(
+                'Floating point adapter not yet implemented'
+            );
 
-            return BcMath::factory($number, $scale);
+        } else {
 
-        // Otherwise, use GMP if we have it, as its fast as hell
-        } elseif (static::isGmpAvailable()) {
-            return Gmp::factory($number, $scale);
-
-        } elseif (static::isBcMathAvailable()) {
-            return BcMath::factory($number, $scale);
+            return Integer::factory($number);
         }
-
-        // TODO: Fall back to native?
-        throw new AdapterSupportException();
     }
 
     /**
