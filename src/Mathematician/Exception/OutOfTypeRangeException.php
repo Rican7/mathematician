2b487879349c649010da27d94e2ac022571ad64b
Trevor Suarez <rican7@gmail.com>
2014-04-25 01:02:18 -0400

    New exception for out of type range errors

---
+++ src/Mathematician/Exception/OutOfTypeRangeException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Mathematician
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2014 Trevor Suarez
+ * @license     MIT
+ * @version     0.1.0
+ */
+
+namespace Mathematician\Exception;
+
+use RangeException;
+
+/**
+ * OutOfTypeRangeException
+ *
+ * @uses RangeException
+ * @uses MathematicianExceptionInterface
+ * @package Mathematician\Exception
+ */
+class OutOfTypeRangeException extends RangeException implements MathematicianExceptionInterface
+{
+
+    /**
+     * Default Properties
+     */
+
+    protected $message = 'The value is outside of the range of the requested type';
+}
